b7a0375144647892a070d53a297eff5e3fa3a7dd
Yaro Schiffelers <yaroschiffelers@gmail.com>
2017-12-08 15:11:41 +0100

    add: image optimizer plugin
    to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,30 @@ let config = {
                     use: ['css-loader', 'sass-loader'],
                     fallback: 'style-loader'
                 })
-            }
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: ['file-loader?context=src/assets/images/&name=images/[path][name].[ext]', {  // images loader
+                    loader: 'image-webpack-loader',
+                    query: {
+                        mozjpeg: {
+                        progressive: true,
+                    },
+                    gifsicle: {
+                        interlaced: false,
+                    },
+                    optipng: {
+                        optimizationLevel: 4,
+                    },
+                    pngquant: {
+                        quality: '75-90',
+                        speed: 3,
+                    },
+                },
+            }],
+            exclude: /node_modules/,
+            include: __dirname,
+            },
         ] 
     },
     plugins: [
@@ -33,7 +56,8 @@ let config = {
         contentBase: path.resolve(__dirname, './public'), // The directory to serve HTML content from
         historyApiFallback: true, // Fallback to index.html for SPA's (404's etc)
         inline: true, // Set to fasle to disable including client scripts (like livereload)
-        open: false // Set to true to open default browser while launching
+        open: false, // Set to true to open default browser while launching
+        compress: true // Enable gzip compression for everything served
     },
     devtool: 'eval-source-map' // Enable devtools (better debugging)
 }
